cd92de243d75a8d3f3e3ecf881fa738265c33691
Raymond Wagenmaker <raymondwagenmaker@gmail.com>
2013-02-27 12:21:26 +0100

    fix sys_argv, don't override console_handler, check for S flag

--- bin/offlineimap-notify.py
+++ bin/offlineimap-notify.py
@@ -3,84 +3,91 @@
 from argparse import ArgumentParser
 from collections import defaultdict
 from contextlib import contextmanager
-import inspect
+#import inspect
 import logging
 import os
-from os import path
+#from os import path
+import re
 import subprocess
 import sys
 
-from offlineimap import OfflineImap
-from offlineimap.ui import UI_LIST, UIBase
+import offlineimap
+from offlineimap.folder.Maildir import MaildirFolder
 try:
     import pynotify
     def notify(summary, body):
-        pynotify.init(path.basename(sys.argv[0]))
+        pynotify.init('OfflineImap')
         pynotify.Notification(summary, body).show()
 except ImportError:
     def notify(summary, body):
         try:
-            subprocess.call(['notify-send', summary, body])
+            subprocess.call(['notify-send', '-a', 'OfflineImap', summary, body])
         except OSError:
             logging.error('failed to send notification')
 
-# TODO: is this necessary/useful?
-ARGS_IGNORE = ('--info', '-u', '--version', '--dry-run', '-P')
-
-def notify_ui(loglevel, console_output=False):
-    class NotifyUI(UIBase.UIBase):
-        def __init__(self, config, loglevel = loglevel):
+def notify_ui(loglevel):
+    class NotifyUI(offlineimap.ui.UIBase.UIBase):
+        def __init__(self, config, loglevel=loglevel):
+            print('init NotifyUI')
             super(NotifyUI, self).__init__(config, loglevel)
-            self.copiedmessages = defaultdict(list)
-
-        _ismaildir = lambda f: isinstance(f, MaildirFolder)
+            self.newmessages = defaultdict(list)
 
         def copyingmessage(self, uid, num, num_to_copy, src, destfolder):
-            if _ismaildir(destfolder):
-                self.copiedmessages[destfolder].append(uid)
-            UIBase.copyingmessage(self, uid, num, num_to_copy, src, destfolder)
+            if (isinstance(destfolder, MaildirFolder) and
+                'S' not in src.getmessageflags(uid)):
+                # TODO: using folder object as key assumes that:
+                #   f1.name == f2.name => id(f1) == id(f2)
+                self.newmessages[destfolder].append(uid)
+            super(NotifyUI, self).copyingmessage(uid, num, num_to_copy, src, destfolder)
 
-        if 'have to print {new}':
-            def skippingfolder
+        def terminate(self, exitstatus=0, errortitle=None, errormsg=None):
+            send_notification(self.newmessages)
+            # TODO: check exceptions, change exitstatus to >0
+            print('exitstatus={}'.format(exitstatus))
+            super(NotifyUI, self).terminate(exitstatus, errortitle, errormsg)
 
-        if not console_output:
-            def setup_consolehandler(self):
-                """Override to disable logging to console."""
-                # TODO: reconsider this feature
-                pass
     return NotifyUI
 
+def send_notification(new_mails):
+    summary = 'New mail in ' + ', '.join(map(str, new_mails.keys()))
+    if len(new_mails) == 1:
+        folder, uids = new_mails.pop_item()
+        summary += ' in ' + folder
+    else:
+        pass
+    notify(summary, 'body')
+
+def parse_args():
+    parser = ArgumentParser(description=__doc__,
+                            epilog='''Additional arguments will be passed on to
+                                   offlineimap, except for --info.''')
+    parser.add_argument('-u', default=logging.WARNING, dest='loglevel',
+                        choices={'basic': logging.INFO, 'quiet': logging.WARNING},
+                        help='''specify (non-interactive) user interface to use
+                             (default: quiet)''')
+    parser.add_argument('-n', help='do not send a notification',
+                        dest='notify', action='store_false')
+    # TODO:
+    # -n defeats the purpose of using this script, until exitstatus is
+    # influenced (and even then it's probably a bit silly)
+    return parser.parse_known_args()
+
 @contextmanager
-def sys_argv(argv):
+def sys_argv(argv, prog=None):
     argv_old = sys.argv
-    sys.argv = argv
+    sys.argv = argv[:]
+    sys.argv.insert(0, prog or argv_old[0])
     yield
     sys.argv = argv_old
 
-def parse_args():
-    fmt_default = " (default: %(default)s)"
-    parser = ArgumentParser(description=__doc__,
-                            epilog='''Additional arguments will be passed
-                                   on to offlineimap, but {} are discarded.
-                                   '''.format(', '.join(ARGS_IGNORE)))
-    parser.add_argument('-p', '--print', dest='format',
-                        help='''print total (not limited to this sync)
-                             number of new and queued mails
-                             (use {new} and/or {queued} in FORMAT)''',
-                        nargs='?', const='new: {new}, queued: {queued}')
-    parser.add_argument('-Q', '--queue', metavar='DIR',
-                        help='location of msmtpq queue, to print {queued}' +
-                             fmt_default,
-                        default=path.join('~', '.msmtp.queue'))
-    parser.add_argument('-n', help='do not send a notification',
-                        dest='notify', action='store_false')
-    return parser.parse_known_args()
-
 if __name__ == '__main__':
     args, offlineimap_args = parse_args()
-    UI_LIST = {'notify': notify_ui(args.loglevel)}
-    offlineimap_args = filter(lambda a: a not in ARGS_IGNORE, offlineimap_args)
+    offlineimap.ui.UI_LIST['notify'] = notify_ui(args.loglevel)
+    try:
+        offlineimap_args.remove('--info')
+    except ValueError:
+        pass
     offlineimap_args.extend(['-u', 'notify'])
-    with sys_argv(offlineimap_args):
-        oi = OfflineImap()
+    with sys_argv(offlineimap_args, prog='offlineimap'):
+        oi = offlineimap.OfflineImap()
         oi.run()
