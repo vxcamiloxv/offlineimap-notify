5680901094c30cfa051b314fe83232a7a58f916a
Raymond Wagenmaker <raymondwagenmaker@gmail.com>
2013-02-28 21:57:49 +0100

    some notes and small improvements

--- bin/offlineimap-notify.py
+++ bin/offlineimap-notify.py
@@ -17,6 +17,8 @@ import sys
 from offlineimap import OfflineImap, ui
 from offlineimap.folder.Maildir import MaildirFolder
 try:  # FIXME: pynotify might still raise GError when no dbus notification service is known
+      # but then notify-send will also be useless unless it's been replaced by
+      # a custom script, so try/except can remain?
     import pynotify
     def send_notification(summary, body):
         pynotify.init('OfflineImap')
@@ -35,7 +37,8 @@ def notify_ui(notify, base_ui):
             super(NotifyUI, self).acct(account)
 
         def acctdone(self, account):
-            notify(self.new_messages)
+            if self.new_messages:
+                notify(self.new_messages)
             del self.new_messages
             super(NotifyUI, self).acctdone(account)
 
@@ -48,13 +51,12 @@ def notify_ui(notify, base_ui):
     return NotifyUI
 
 def notify(options, new_mails):
-    account = None
     count = 0
     body = []
     for folder, uids in new_mails.iteritems():
-        account = account or folder.accountname()
         count += len(uids)
         body.append('{} in {}'.format(len(uids), folder))
+    account = folder.accountname()
 
     if count > options.max:
         summary = 'New mail for {} ({})'.format(account, count)
@@ -63,8 +65,8 @@ def notify(options, new_mails):
     need_body = '{body' in options.body or '{body' in options.summary
     parser = Parser()
     for folder, uids in new_mails.iteritems():
+        format_args = {'account': account, 'folder': folder}
         for uid in uids:
-            format_args = {'account': account, 'folder': folder}
             message = parser.parsestr(folder.getmessage(uid),
                                       headersonly=not need_body)
             timestamp = mktime_tz(parsedate_tz(message['date']))
@@ -73,8 +75,11 @@ def notify(options, new_mails):
             if need_body:
                 for part in message.walk():
                     if part.get_content_type() == 'text/plain':
-                        format_args['body'] = part
+                        format_args['body'] = part  # FIME: need to .get_payload(decode=True),
+                        # but should study multipart handling more too
                         break
+                else:
+                    format_args['body'] = 'FIXME'
             try:
                 send_notification(options.summary.format(**format_args),
                                   options.body.format(**format_args))
@@ -86,28 +91,27 @@ def notify(options, new_mails):
 def parse_args():
     def getdefaultloglevel(ui_name):
         argspec = inspect.getargspec(ui.UI_LIST[ui_name].__init__)
-        defaults = dict(zip(reversed(argspec.args), argspec.defaults or ()))
-        try:
-            return defaults['loglevel']
-        except KeyError:
-            return logging.NOTSET
+        defaults = dict(zip(reversed(argspec.args), reversed(argspec.defaults or [])))
+        return defaults.get('loglevel', logging.NOTSET)
 
-    fmt_default = ' (default: %(default){})'.format
+    format_default = ' (default: %(default){})'.format
     parser = ArgumentParser(description=__doc__,
                             epilog='''Additional arguments will be passed on to
                                    offlineimap, except for --info.''')
     parser.add_argument('-u', default=max(ui.UI_LIST, key=getdefaultloglevel),
                         choices=ui.UI_LIST.keys(), dest='ui',
                         help='specify (non-interactive) user interface to use' +
-                             fmt_default('s'))
+                             format_default('s'))
     parser.add_argument('-s', '--summary',
-                        help='format for notification summary' + fmt_default('r'),
-                        default='New mail for {account}')
+                        help='format for notification summary' + format_default('r'),
+                        default='New mail for {account} in {folder}')
     parser.add_argument('-b', '--body',
-                        help='format for notification body' + fmt_default('r'),
-                        default='From: {hdr[from]}\nSubject: {hdr[subject]}')
+                        help='format for notification body' + format_default('r'),
+                        default='From: {h[from]}\nSubject: {h[subject]}')
     parser.add_argument('-m', '--max', default=2,
-                        help='maximum number of messages' + fmt_default('s'))
+                        help='''maximum number of notifications; when account
+                             has more new messages, send one summary
+                             notification''' + format_default('s'))
     args, offlineimap_args = parser.parse_known_args()
     try:
         offlineimap_args.remove('--info')
@@ -118,12 +122,18 @@ def parse_args():
 
 @contextmanager
 def sys_argv(argv, prog=None):
+    """"""
     argv_old = sys.argv
     sys.argv = [argv_old[0] if prog is None else prog] + argv
     yield
     sys.argv = argv_old
 
 def disable_interactive_uis():
+    """Make UIs not defined in the Noninteractive module unavailable.
+
+    Modify offlineimap.ui.UI_LIST in place, removing interactive UIs.
+    
+    """
     is_noninteractive = lambda (_, cls): cls.__module__ == ui.Noninteractive.__name__
     noninteractive_uis = filter(is_noninteractive, ui.UI_LIST.iteritems())
     ui.UI_LIST.clear()
